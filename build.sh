556cc3b84335b01421a905a23e4b5ce593f7ffdc
Martin Probst <martin@probst.io>
2016-06-12 11:34:32 -0700

    rel: 1.0.3.

--- build.sh
+++ build.sh
@@ -4,5 +4,7 @@ set -e
 set -x
 
 tsc
+svg2png -h 128 -w 128 res/icon.svg res/icon128.png
+svg2png -h 48 -w 48 res/icon.svg res/icon48.png
 rm instametric.zip || true
 zip instametric.zip manifest.json metrify.js res/*.png
