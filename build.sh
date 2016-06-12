f8e067a44df9ee3026847e938df6663d080c2ab7
Martin Probst <martin@probst.io>
2016-06-12 11:44:35 -0700

    Crush pngs before publishing.
    Does not make a substantial difference, but might have gotten publishing unstuck?

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -x
 
 tsc
 svg2png -h 128 -w 128 res/icon.svg res/icon128.png
+pngcrush -ow res/icon128.png
 svg2png -h 48 -w 48 res/icon.svg res/icon48.png
+pngcrush -ow res/icon48.png
 rm instametric.zip || true
 zip instametric.zip manifest.json metrify.js res/*.png
